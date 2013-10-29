dc9b74ad37e9f806b4c831ad80cc5eb48976b3ee
Aviv Eyal <avivey@gmail.com>
2013-10-28 22:23:13 -0700

    progress, refactor

--- GithubApiCallFuture.php
+++ GithubApiCallFuture.php
@@ -7,10 +7,7 @@ class GithubApiCallFuture extends FutureProxy {
   private $uri;
   private $future;
 
-  public function __construct($api, PhabricatorUser $user) {
-    // parent::__construct();
-
-
+  public static function getAccessToken(PhabricatorUser $user) {
     $account = id(new PhabricatorExternalAccountQuery())
       ->setViewer($user)
       ->withUserPHIDs(array($user->getPHID()))
@@ -19,12 +16,18 @@ class GithubApiCallFuture extends FutureProxy {
 
     $github_provider = PhabricatorAuthProvider::getEnabledProviderByKey(
       self::GITHUB_PROVIDER_KEY); // todo get this from somewhere.
-    $access_token = $github_provider->getOAuthAccessToken($account);
+    return $github_provider->getOAuthAccessToken($account);
+  }
 
+  public function __construct($api, $access_token, array $data = null) {
     $this->uri = new PhutilURI(self::BASE_URI . $api);
     $this->uri->setQueryParam('access_token', $access_token);
     $this->future = new HTTPSFuture($this->uri);
     $this->setProxiedFuture($this->future);
+
+    if ($data !== null) {
+      $this->setData(json_encode($data));
+    }
   }
 
   protected function didReceiveResult($result){
@@ -35,6 +38,6 @@ class GithubApiCallFuture extends FutureProxy {
     $this->future->setMethod($method);
   }
   public function setData($encoded_data) {
-    $future->setData($encoded_data);
+    $this->future->setData($encoded_data);
   }
 }
--- PushToGitFindDetailsController.php
+++ PushToGitFindDetailsController.php
@@ -5,10 +5,7 @@ class PushToGitFindDetailsController extends AvivUtilController {
     $request = $this->getRequest();
     $viewer = $request->getUser();
 
-    $pwd = '/home/avive/code/test-repo';
-
-    $api = new GithubApiCallFuture('', $viewer);
-    $api->start();
+    $pwd = '/home/avive/code/test-repo/';
 
     $future = new ExecFuture(
       '/bin/bash -c %s',
@@ -21,13 +18,118 @@ class PushToGitFindDetailsController extends AvivUtilController {
     $dd['parsed'] = $parsed;
 
     $new_blobs = array();
-    foreach ($parsed as $path => $data) {
+    foreach ($parsed as $data) {
       if ($data['type'] === 'blob') {
-        $new_blobs[$path] = $data;
+        $new_blobs[$data['sha']] = $data;
       }
     }
 
+    $access_token = GithubApiCallFuture::getAccessToken($viewer);
+    $futures = array();
+    foreach ($new_blobs as $blob) {
+      // $content = file_get_contents($pwd.$blob['path']);
+      // $content = base64_encode($content);
+      // $future =
+       // self::makePushBlobFuture('avivey/test-repo', $content, $access_token);
+      // $futures[$blob['sha']] = $future;
+    }
+
+    $parsed = array();
+    foreach ($futures as $hash => $future) {
+      $res = self::parseGithubApiFuture($future);
+      $res['WIN'] = idx($res['body'], 'sha') == $hash;
+      $parsed[$hash] = $res;
+    }
+    $dd['creat blobs call'] = $parsed;
+
+    $future = new ExecFuture(
+      '/bin/bash -c %s',
+      'git cat-file commit HEAD');
+    $future->setCwd($pwd);
+    $commit_info = $future->resolvex();
+    $commit_info = $commit_info[0];
+
+    $commit_info = $this->parseCommit($commit_info);
+    $dd['wanted tree hash'] = $commit_info;
+
+    $future = new ExecFuture(
+      '/bin/bash -c %s',
+      'git ls-tree -r HEAD');
+    $future->setCwd($pwd);
+    $head_tree = $future->resolvex();
+    $head_tree = $this->parseGitTree($head_tree[0]);
+    $dd['tree'] = json_encode($head_tree);
+
+    $future = $this->makeCreateTreeFuture('avivey/test-repo', $head_tree, $access_token);
+    $dd['create tree call'] = self::parseGithubApiFuture($future);
+
+    $future = $this->makeCommitFuture(
+      'avivey/test-repo',
+      $commit_info['message'],
+      $commit_info['tree'],
+      $commit_info['parent'],
+      $access_token
+      );
+    $dd['create make commit'] = self::parseGithubApiFuture($future);
+
     return $this->buildHumanReadableResponse($dd);
+  }
+
+  function makeCommitFuture($repo, $message, $tree, $parent, $access_token) {
+    // todo add committer data
+    // todo get information from phabricator commit object.
+    $data = array(
+      'message' => $message,
+      'tree' => $tree,
+      'parents' => array($parent),
+      // todo parse author info + time.
+      'author' => array(
+        'name' => 'Aviv Eyal',
+        'email' => 'avivey@gmail.com',
+        'date' => '2013-10-27T17:10:40-0700',
+      ),
+    );
+    $future = new GithubApiCallFuture(
+      "repos/$repo/git/commits",
+      $access_token,
+      $data);
+    $future->setMethod('POST');
+
+    return $future->start();
+  }
+
+  function makeCreateTreeFuture($repo, $tree, $access_token) {
+    $data = array('tree' => $tree);
+
+    $future = new GithubApiCallFuture(
+      "repos/$repo/git/trees",
+      $access_token,
+      $data);
+    $future->setMethod('POST');
+    return $future->start();
+  }
+
+  function makePushBlobFuture($repo, $blob_in_base64, $access_token) {
+    $data = array(
+      'encoding' => 'base64',
+      'content' => $blob_in_base64,
+    );
+    $future = new GithubApiCallFuture(
+      "repos/$repo/git/blobs",
+      $access_token,
+      $data);
+    $future->setMethod('POST');
+    $future->start();
+    return $future;
+  }
+
+  function parseGithubApiFuture($future) {
+    list($status, $body, $headers) = $future->resolve();
+
+    $dd['api status'] = $status->getStatusCode();
+//    $dd['headers'] = $headers;
+    $dd['body'] = json_decode($body, true);
+    return $dd;
   }
 
   private function parseGitTree($stdout) {
@@ -48,12 +150,35 @@ class PushToGitFindDetailsController extends AvivUtilController {
       if (!$ok) {
         throw new Exception("Failed to parse git ls-tree output!");
       }
-      $result[$matches[4]] = array(
+      // this now matches github's api.
+      $result[] = array(
         'mode' => $matches[1],
         'type' => $matches[2],
-        'ref'  => $matches[3],
+        'sha'  => $matches[3],
+        'path' => $matches[4],
       );
     }
     return $result;
+  }
+
+  function parseCommit($commit) {
+    $commit = trim($commit); // I hope.
+    $text = explode("\n", $commit);
+    $data = array();
+    $message = array();
+    $in_message = false;
+    while(!empty($text)) {
+      $line = array_shift($text);
+      if ($in_message) {
+        array_push($message, $line);
+      } else if (empty($line)) {
+        $in_message = true;
+      } else {
+        $line = explode(' ', $line, 2);
+        $data[$line[0]] = $line[1];
+      }
+    }
+    $data['message'] = implode("\n", $message);
+    return $data;
   }
 }
